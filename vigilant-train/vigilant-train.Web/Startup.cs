f7924e3dd92e8b3df0f7b4e592181c347e4a1df6
Keith <keithtmccartney@hotmail.com>
2020-10-12 10:27:40 +0100

    Adding a Payment Detail model and database context, applying changes to the 'appsettings' for a connection string, and changing the service configuration on Startup to use this new SQL server. Migration to be ran in next commit.

--- vigilant-train/vigilant-train.Web/Startup.cs
+++ vigilant-train/vigilant-train.Web/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using vigilant_train.Web.Models;
 
 namespace vigilant_train.Web
 {
@@ -26,6 +28,8 @@ namespace vigilant_train.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<PaymentDetailContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
