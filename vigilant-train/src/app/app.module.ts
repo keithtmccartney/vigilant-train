a9dfd9bc15a57f42e78fc88f645fb21d4f2de76e
Keith <keithtmccartney@hotmail.com>
2020-10-12 11:09:24 +0100

    An Angular project was previously created ("ng new vigilant-train") and served ("ng serve --o"), and now two new Components have been created ("ng g c payment-details -s --skipTests", "ng g c payment-details/payment-detail-form -s --skipTests") with flags to skip separate component-specific style sheet and to skip test files with the extension '.spec.ts'.

--- vigilant-train/src/app/app.module.ts
+++ vigilant-train/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailFormComponent
   ],
   imports: [
     BrowserModule
