5f7def3dd801b77d689277eeee751c22c0625f7c
Keith <keithtmccartney@hotmail.com>
2020-10-12 16:38:13 +0100

    Fixing a typo on the input attributes for Card Owner Name, also importing the Toastr usage into the main App module and commenting-out the Browser Animations module as it's playing havoc with the new Ivy compiler.

--- vigilant-train/src/app/app.module.ts
+++ vigilant-train/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
+import { PaymentDetailService } from './shared/payment-detail.service';
 
 import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms';
+
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -16,11 +22,13 @@ import { FormsModule } from '@angular/forms';
     PaymentDetailFormComponent
   ],
   imports: [
+    BrowserModule,
+    // BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    BrowserModule
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [PaymentDetailService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
