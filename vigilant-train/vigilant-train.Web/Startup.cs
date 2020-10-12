ed8a39374a5e0eb1a5bf3975cd6608fea25b4ee8
Keith <keithtmccartney@hotmail.com>
2020-10-12 14:49:25 +0100

    Adding CORS accessibility into the Startup configuration.

--- vigilant-train/vigilant-train.Web/Startup.cs
+++ vigilant-train/vigilant-train.Web/Startup.cs
@@ -30,11 +30,23 @@ namespace vigilant_train.Web
             services.AddControllers();
 
             services.AddDbContext<PaymentDetailContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                options.JsonSerializerOptions.DictionaryKeyPolicy = null;
+            });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.WithOrigins()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
